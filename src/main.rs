469cb0902545c9c7a562598ba656d04709be0384
Yannis Matezki <git@yannismate.de>
2021-10-31 17:28:44 +0100

    implement some actix_web and actix stuff

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,36 @@
 mod twitch;
 
-use std::{thread, time};
-use crate::twitch::chat::IRCClient;
+use actix_web::{App, HttpServer, middleware, Responder, HttpResponse, get};
+use crate::twitch::chat::{IrcClientActor, IrcConnect, IrcAddListener};
+use actix::{Actor, Addr};
 
-#[tokio::main]
-pub async fn main() {
-  println!("Hello, world!");
-  let bits_closure = |amount_bits: u64| {
-    println!("Bits {:1}", amount_bits);
+pub struct AppState {
+  irc_client: Addr<IrcClientActor>,
+}
+
+#[get("/")]
+async fn hello(data: actix_web::web::Data<AppState>) -> impl Responder {
+  let res = data.irc_client.send(IrcConnect(String::from("justinfan523"), None)).await;
+  HttpResponse::Ok().body(format!("{:?}", res))
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+  let state = AppState {
+    irc_client: IrcClientActor::default().start()
   };
 
-  let subs_closure = |amount_subs: u8| {
-    println!("Subs {:1}", amount_subs);
-  };
 
-  let client = IRCClient::new("justintv392", None, bits_closure, subs_closure).await;
+  let state = actix_web::web::Data::new(state);
 
+  HttpServer::new(move || {
+    App::new()
+      .wrap(middleware::Logger::default())
+      .app_data(state.clone())
+      .service(hello)
+  }).bind("127.0.0.1:8080")?.run().await
 
-  //only works when IRCClient is supplied with an oauth token
-  let can_send_msg = client.send_msg("yannismate", "test").await.is_ok();
-  println!("Can send msg? {:?}", can_send_msg);
-
-
-  //keepalive until http server is implemented
-  loop {
-    thread::sleep(time::Duration::from_secs(5));
-  }
 
 }
--- src/twitch/chat.rs
+++ src/twitch/chat.rs
@@ -1,107 +1,163 @@
+use actix::prelude::*;
+use actix::dev::MessageResult;
 use twitch_irc::{ClientConfig, TwitchIRCClient, SecureTCPTransport};
 use twitch_irc::login::StaticLoginCredentials;
-use twitch_irc::message::ServerMessage;
-use twitch_irc::message::UserNoticeEvent;
-use std::convert::TryFrom;
-use std::time::Duration;
 use expiring_map::ExpiringMap;
-use std::error::Error;
+use std::time::Duration;
+use tokio::sync::watch::{Sender, Receiver};
+use twitch_irc::message::{ServerMessage, UserNoticeEvent};
+use std::sync::{Arc, Mutex};
 
-pub struct IRCClient {
-  client: TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>,
-  is_logged_in: bool
+pub struct IrcClientActor {
+  client: Option<TwitchIRCClient<SecureTCPTransport, StaticLoginCredentials>>,
+  is_logged_in: bool,
+  channel_tx: Arc<Mutex<Option<Sender<(u64, u64)>>>>,
+  channel_rx: Arc<Mutex<Option<Receiver<(u64, u64)>>>>
 }
 
-impl IRCClient {
-  pub async fn new<F: Fn(u64) + 'static + Sync + Send, G: Fn(u8) + 'static + Sync + Send>(username: &str, token: Option<&str>, bits_closure: F, subs_closure: G) -> IRCClient {
+impl Default for IrcClientActor {
+  fn default() -> Self {
+    IrcClientActor {
+      client: None,
+      is_logged_in: false,
+      channel_tx: Arc::new(Mutex::new(None)),
+      channel_rx: Arc::new(Mutex::new(None))
+    }
+  }
+}
 
-    let config = match token {
-      Some(token) => ClientConfig::new_simple(StaticLoginCredentials::new(username.to_string(), Some(token.to_string()))),
-      None => ClientConfig::new_simple(StaticLoginCredentials::new("justinfan5133".to_string(), None))
-    };
+impl Actor for IrcClientActor {
+  type Context = Context<Self>;
+}
 
 
-    let (mut incoming_messages, client) =
-      TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+/*
+  IRC Connect
+*/
+#[derive(Message, Debug)]
+#[rtype(result = "IrcConnectResponse")]
+pub struct IrcConnect(pub String, pub Option<String>);
+
+#[derive(Debug)]
+pub enum IrcConnectResponse {
+  Success,
+  ConnectionFailed,
+  AuthFailed
+}
+
+
+impl Handler<IrcConnect> for IrcClientActor {
+  type Result = MessageResult<IrcConnect>;
+
+  fn handle(&mut self, msg: IrcConnect, _ctx: &mut Self::Context) -> Self::Result {
+
+    println!("{:?}", &msg);
+    let config = ClientConfig::new_simple(StaticLoginCredentials::new(msg.0, msg.1));
+    let (mut msg_rcvr, client) = TwitchIRCClient::new(config);
+
+    //TODO: check connection state
+
+    self.client = Some(client);
+
+    let tx = self.channel_tx.clone();
 
     tokio::spawn(async move {
+      let mut gift_bombs = ExpiringMap::<String, u64>::new(Duration::from_secs(30));
 
-      let mut gift_bombs = ExpiringMap::new(Duration::from_secs(30));
+      //move tx into closure
+      let tx = tx;
 
-      while let Some(message) = incoming_messages.recv().await {
-        match message {
+      while let Some(msg) = msg_rcvr.recv().await {
+        if tx.lock().map_or(true, |tx|{tx.is_none()}) { continue; }
+
+        match msg {
           ServerMessage::Privmsg(priv_msg) => {
-            match priv_msg.bits {
-              Some(amount) => {
-                bits_closure(amount);
-              },
-              None => {}
+            if let Some(bits) = priv_msg.bits {
+              let locked_tx = tx.lock().unwrap();
+              if locked_tx.is_some() {
+               locked_tx.as_ref().unwrap().send((0, bits)).unwrap_or_else(|err| println!("{:?}", err));
+              }
             }
           },
           ServerMessage::UserNotice(notice) => {
             match notice.event {
-              UserNoticeEvent::SubMysteryGift {mass_gift_count, ..} => {
-                //Multiple gifted subs
-                gift_bombs.insert(notice.sender.id, mass_gift_count);
-                subs_closure(u8::try_from(mass_gift_count).unwrap_or(0));
+              UserNoticeEvent::SubOrResub { .. } => {
+                //Single sub or resub
+                let locked_tx = tx.lock().unwrap();
+                if locked_tx.is_some() {
+                  locked_tx.as_ref().unwrap().send((1, 0)).unwrap_or_else(|err| println!("{:?}", err));
+                }
               },
-              UserNoticeEvent::AnonSubMysteryGift {mass_gift_count, ..}=> {
-                //Multiple gifted subs
-                gift_bombs.insert("274598607".to_string(), mass_gift_count);
-                subs_closure(u8::try_from(mass_gift_count).unwrap_or(0));
-              },
-              UserNoticeEvent::SubGift {is_sender_anonymous, ..} => {
-                let sender_id = match is_sender_anonymous {
-                  true => "274598607".to_string(),
-                  false => notice.sender.id
-                };
-                //Single gifted sub, can be part of a sub-bomb
+              UserNoticeEvent::SubGift { is_sender_anonymous, .. } => {
+                let sender_id = if is_sender_anonymous {String::from("274598607")} else {notice.sender.id};
                 if *gift_bombs.get(&sender_id).unwrap_or(&0) > 0 {
-                  //Part of the sub bomb
+                  //Sub gift part of bigger sub bomb, ignore
                   let gift_bomb_left = gift_bombs.get(&sender_id).unwrap_or(&0);
                   let gift_bomb_left = gift_bomb_left - 1;
                   gift_bombs.insert(sender_id, gift_bomb_left);
                 } else {
-                  //Single gift
-                  subs_closure(1);
+                  //Single sub gift, not part of sub bomb
+                  let locked_tx = tx.lock().unwrap();
+                  if locked_tx.is_some() {
+                    locked_tx.as_ref().unwrap().send((1, 0)).unwrap_or_else(|err| println!("{:?}", err));
+                  }
                 }
               },
-              UserNoticeEvent::SubOrResub {..} => {
-                //Normal sub or resub
-                subs_closure(1);
+              UserNoticeEvent::SubMysteryGift { mass_gift_count, .. } => {
+                //Gift bomb, insert sender into map and pass on total amount
+                gift_bombs.insert(notice.sender.id, mass_gift_count);
+                let locked_tx = tx.lock().unwrap();
+                if locked_tx.is_some() {
+                  locked_tx.as_ref().unwrap().send((mass_gift_count, 0)).unwrap_or_else(|err| println!("{:?}", err));
+                }
+              },
+              UserNoticeEvent::AnonSubMysteryGift { mass_gift_count, .. } => {
+                //Anon gift bomb, insert anon id into map and pass on total amount
+                gift_bombs.insert(String::from("274598607"), mass_gift_count);
+                let locked_tx = tx.lock().unwrap();
+                if locked_tx.is_some() {
+                  locked_tx.as_ref().unwrap().send((mass_gift_count, 0)).unwrap_or_else(|err| println!("{:?}", err));
+                }
               },
               _ => {}
-            };
-
+            }
           },
           _ => {}
         }
       }
+
     });
+    self.client.as_ref().unwrap().join(String::from("yannismate"));
 
-    IRCClient {
-      client,
-      is_logged_in: token.is_some()
+    MessageResult(IrcConnectResponse::Success)
+  }
+}
+
+
+/*
+  Add Listener
+*/
+#[derive(Message, Debug)]
+#[rtype(result = "IrcAddListenerResponse")]
+pub struct IrcAddListener;
+
+#[derive(Debug)]
+pub struct IrcAddListenerResponse {
+  pub receiver: Receiver<(u64, u64)>
+}
+
+impl Handler<IrcAddListener> for IrcClientActor {
+  type Result = MessageResult<IrcAddListener>;
+
+  fn handle(&mut self, msg: IrcAddListener, ctx: &mut Self::Context) -> Self::Result {
+    let mut rx_lock = self.channel_rx.lock().unwrap();
+    if rx_lock.is_none() {
+      let mut tx_lock = self.channel_tx.lock().unwrap();
+      let (tx, rx) = tokio::sync::watch::channel((0 as u64, 0 as u64));
+      *tx_lock = Some(tx);
+      *rx_lock = Some(rx);
     }
-
+    let rx_copy = rx_lock.clone().unwrap().clone();
+    MessageResult(IrcAddListenerResponse {receiver: rx_copy})
   }
-
-  pub async fn join_channel(&self, channel: &str) {
-    self.client.join(channel.to_string());
-  }
-
-  pub async fn part_channel(&self, channel: &str) {
-    self.client.part(channel.to_string());
-  }
-
-  pub async fn send_msg(&self, channel: &str, msg: &str) -> Result<(), Box<dyn Error>> {
-    if !self.is_logged_in {
-      Err("IRC Client is not logged in.")?
-    }
-    match self.client.say(channel.to_string(), msg.to_string()).await {
-      Ok(()) => Result::Ok(()),
-      Err(reason) => Err(&*reason.to_string())?
-    }
-  }
-
 }
