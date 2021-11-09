b32004ced98d3a0a4b878e51120cdd933554e31e
Yannis Matezki <git@yannismate.de>
2021-11-10 00:49:29 +0100

    more work on twitch irc

--- src/twitch/chat.rs
+++ src/twitch/chat.rs
@@ -1,13 +1,16 @@
+use std::net::TcpStream;
 use actix::prelude::*;
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, Mutex, RwLock};
 use websocket::{ClientBuilder, OwnedMessage};
-use crate::twitch::irc_parse::IrcCommand;
+use websocket::client::sync::Writer;
+use crate::twitch::irc_parse::{IrcCapabilities, IrcCommand, IrcMessage};
 
 const TWITCH_IRC_URL: &'static str = "ws://irc-ws.chat.twitch.tv:80";
 
 pub struct IrcClientActor {
   is_logged_in: bool,
-  cc_subscribers: Arc<RwLock<Vec<Recipient<IrcCcEvent>>>>
+  cc_subscribers: Arc<RwLock<Vec<Recipient<IrcCcEvent>>>>,
+  sender: Arc<Mutex<Option<Writer<TcpStream>>>>
 }
 
 #[derive(Message, Clone)]
@@ -21,7 +24,8 @@ impl Default for IrcClientActor {
   fn default() -> Self {
     IrcClientActor {
       is_logged_in: false,
-      cc_subscribers: Arc::new(RwLock::new(Vec::<Recipient<IrcCcEvent>>::new()))
+      cc_subscribers: Arc::new(RwLock::new(Vec::<Recipient<IrcCcEvent>>::new())),
+      sender: Arc::new(Mutex::new(None))
     }
   }
 }
@@ -58,6 +62,12 @@ impl Handler<IrcConnect> for IrcClientActor {
   type Result = MessageResult<IrcConnect>;
 
   fn handle(&mut self, msg: IrcConnect, _ctx: &mut Self::Context) -> Self::Result {
+    {
+      let sender_lock = self.sender.lock().unwrap();
+      if sender_lock.is_some() {
+        sender_lock.as_ref().unwrap().shutdown_all().unwrap_or_else(|err| println!("Could not shutdown old websocket: {:?}", err))
+      }
+    }
 
     println!("{:?}", &msg);
 
@@ -71,7 +81,15 @@ impl Handler<IrcConnect> for IrcClientActor {
     }
     let client = client.unwrap();
 
-    let (mut receiver, mut sender) = client.split().unwrap();
+    let (mut receiver, raw_sender) = client.split().unwrap();
+
+    //Update sender
+    let mut sender = self.sender.lock().unwrap();
+    *sender = Some(raw_sender);
+
+    let sender_handle = self.sender.clone();
+
+    let mut sender = sender.as_mut().unwrap();
 
     if msg.1.is_some() {
       self.is_logged_in = true;
@@ -89,24 +107,53 @@ impl Handler<IrcConnect> for IrcClientActor {
     }
     match first_msg.unwrap() {
       OwnedMessage::Text(text) => {
-        //Should be improved to be future-safe
-        if text.contains("authentication failed") {
-          return MessageResult(IrcConnectResult::AuthFailed);
+        if let Ok(msg) = IrcMessage::parse(text) {
+          match &msg {
+            IrcMessage::Notice(notice) => {
+              if notice.msg.starts_with("Login authentication") {
+                return MessageResult(IrcConnectResult::AuthFailed);
+              }
+            },
+            _ => {}
+          };
+        } else {
+          return MessageResult(IrcConnectResult::ConnectionFailed);
         }
       },
       _ => {
         return MessageResult(IrcConnectResult::ConnectionFailed);
       }
     };
-    std::thread::spawn(move || {
-      while let Ok(msg) = receiver.recv_message() {
 
-        println!("{:?}", msg);
+    std::thread::spawn(move || {
+      let sender_handle = sender_handle;
+      while let Ok(msg) = receiver.recv_message() {
+        match msg {
+          OwnedMessage::Text(text) => {
+            for line in text.lines() {
+              if line.starts_with("PING") {
+                sender_handle.lock().unwrap().as_mut().unwrap().send_message(&to_msg(&String::from("PONG :tmi.twitch.tv"))).unwrap();
+                continue;
+              }
+              let msg = IrcMessage::parse(String::from(line)).unwrap();
+              println!("[IRC] {:?}", msg);
+            }
+          },
+          _ => {}
+        }
       }
       println!("[WS] Close read loop");
     });
 
-  MessageResult(IrcConnectResult::Success)
+    if sender.send_message(&IrcCommand::CapReq(IrcCapabilities::Commands).to_message()).is_err() {
+      return MessageResult(IrcConnectResult::ConnectionFailed);
+    }
+
+    if sender.send_message(&IrcCommand::CapReq(IrcCapabilities::Tags).to_message()).is_err() {
+      return MessageResult(IrcConnectResult::ConnectionFailed);
+    }
+
+    MessageResult(IrcConnectResult::Success)
   }
 }
 
--- src/twitch/irc_parse.rs
+++ src/twitch/irc_parse.rs
@@ -1,15 +1,16 @@
-use std::fmt::{Display, Formatter};
+use std::collections::HashMap;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
 use websocket::Message;
-
 
 //Sending
 pub enum IrcCommand {
-  Nick(NICK),
-  Pass(PASS),
-  Join(JOIN),
-  Part(PART),
-  Privmsg(PRIVMSG),
-  CapReq(CAPREQ),
+  Nick(NICKCMD),
+  Pass(PASSCMD),
+  Join(JOINCMD),
+  Part(PARTCMD),
+  Privmsg(PRIVMSGCMD),
+  CapReq(CAPREQCMD),
   Raw(String)
 }
 
@@ -35,15 +36,16 @@ impl IrcCommand {
   }
 }
 
-pub type NICK = String;
-pub type PASS = String;
-pub type JOIN = String;
-pub type PART = String;
-pub struct PRIVMSG {
+pub type NICKCMD = String;
+pub type PASSCMD = String;
+pub type JOINCMD = String;
+pub type PARTCMD = String;
+pub type CAPREQCMD = IrcCapabilities;
+
+pub struct PRIVMSGCMD {
   channel: String,
   msg: String
 }
-pub type CAPREQ = IrcCapabilities;
 
 pub enum IrcCapabilities {
   Membership,
@@ -61,9 +63,83 @@ impl Display for IrcCapabilities {
 }
 
 //Receiving
+#[derive(Debug)]
 pub enum IrcMessage {
-  Notice(NOTICE),
-  Usernotice(USERNOTICE),
-  Privmsg(PRIVMSG),
+  Notice(IrcMessageContent),
+  Usernotice(IrcMessageContent),
+  Privmsg(IrcMessageContent),
+  Ping,
   Other
 }
+
+impl IrcMessage {
+  pub fn parse(str: String) -> Result<IrcMessage, Box<dyn Error>> {
+    println!("Parsing {:?}", &str);
+    if str.is_empty() {}
+    let mut tags: HashMap<String, String> = HashMap::new();
+    let mut str = str.chars().into_iter().peekable();
+
+    if str.peek().is_some() && str.peek().unwrap().eq(&'@') {
+      let tag_str : String = str.by_ref().take_while(|a| {a.ne(&' ')}).collect();
+      let tag_str : String = tag_str.chars().skip(1).take(tag_str.len() - 1).collect();
+
+      for tag in tag_str.split(';') {
+        let mut spl = tag.split('=');
+        let name = spl.next().unwrap_or("");
+        let val = spl.next().unwrap_or("");
+        tags.insert(String::from(name), String::from(val));
+      }
+    }
+
+
+    let user : String = str.by_ref().skip(1).take_while(|c| {c.ne(&' ')}).collect();
+    let user = if user.contains('!') {Some(String::from(user.split('!').next().unwrap()))} else {None};
+
+    let msg_type : String = str.by_ref().take_while(|a| {a.ne(&' ')}).collect();
+
+    let channel : String = str.by_ref().skip(1).take_while(|a| {a.ne(&' ')}).collect();
+    let channel = if channel.len() > 0 {Some(channel)} else {None};
+
+    let msg : String = str.skip_while(|a| {a.ne(&':')}).skip(1).collect();
+
+    let content = IrcMessageContent{
+      tags,
+      user,
+      channel,
+      msg
+    };
+
+    match &*msg_type {
+      "PRIVMSG" => Ok(IrcMessage::Privmsg(content)),
+      "NOTICE" => Ok(IrcMessage::Notice(content)),
+      "USERNOTICE" => Ok(IrcMessage::Usernotice(content)),
+      "PING" => Ok(IrcMessage::Ping),
+      _ => Ok(IrcMessage::Other)
+    }
+
+  }
+}
+
+#[derive(Debug)]
+pub struct IrcMessageContent {
+  pub tags: HashMap<String, String>,
+  pub user: Option<String>,
+  pub channel: Option<String>,
+  pub msg: String
+}
+
+pub struct IrcParseError;
+
+impl Debug for IrcParseError {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    f.write_str("Error parsing IRC Message")
+  }
+}
+
+impl Display for IrcParseError {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    f.write_str("Error parsing IRC Message")
+  }
+}
+
+impl Error for IrcParseError {}
